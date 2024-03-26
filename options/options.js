4e69232c8892d0472c03ad2c1a3c9d80c4955af3
Crowdbotics <engineering@crowdbotics.com>
2024-03-26 18:21:27 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "jolly-wave-dev-134650",
+  url: "https://jolly-wave-dev-134650.botics.co",
+  api: "https://jolly-wave-dev-134650.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
